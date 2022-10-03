463af1e2b7be79adf7cca5eb5137b2cd7cc0fbbe
Yui_Sakakibara <aja.w@icloud.com>
2022-10-03 15:51:26 +0900

    修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,15 @@
 class UsersController < ApplicationController
   def index
     @users = User.all
-    binding.pry
+    # binding.pry
     # render json: @users
   end
 
   def show
     @user = User.find(params[:id])
+  end
+
+  def create
+
   end
 end
